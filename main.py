695780e96bcf1994c4b302a9f64e226ef226f7f4
Grimaldi Cuaical Gómez <48415689+grimaldi2706@users.noreply.github.com>
2023-02-03 11:12:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,17 @@ def incio():
 
 @app.route('/ver_data', methods=["POST"])
 def ver_data():
+  """
+  Esta función se activa cuando se hace una solicitud HTTP POST a la ruta '/ver_data'.
+  """
   if request.method == 'POST':
+    # Obtiene la consulta y la cantidad desde los datos del formulario enviados en la solicitud HTTP POST.
     query = request.form["consulta"]
     valor = int(request.form["cantidad"])
+    # Llama a la función get_tweets de la libreria gettweets para obtener los tweets según la consulta y la cantidad especificadas.
     df = gettweets.get_tweets(query, limit=valor)
     df_u = df[['username']].reset_index(drop=True)
+  # Renderiza la plantilla 'ver_data.html' y pasa las variables df (con los datos de los tweets) y df_u (con la descripción estadística de los usuarios) a la plantilla.
   return render_template("ver_data.html",
                          tabla=df.to_html(),
                          df_u=df.describe().to_html())
